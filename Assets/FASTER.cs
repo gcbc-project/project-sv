3be4a1dd471d9fd47a1cbbba152f6b8113e7de65
skku-sleepwalk <lucas04112786@gmail.com>
2024-09-14 18:44:12 +0900

    timeScale

---
+++ Assets/FASTER.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FASTER : MonoBehaviour
+{
+    int imgIndex = 0;
+   public List<Sprite> imgList = new List<Sprite>(); 
+    public void ChangeTimeX()
+    {
+        imgIndex++;
+        if (imgIndex > 2) imgIndex -= 3;
+        gameObject.GetComponent<Image>().sprite = imgList[imgIndex];
+        //시간배속 증가 함수
+    }
+}

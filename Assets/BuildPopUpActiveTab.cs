b5ea10aa9c9b73403c2543c24af5c06b675a6f37
skku-sleepwalk <lucas04112786@gmail.com>
2024-09-14 23:32:27 +0900

    ui3

--- Assets/BuildPopUpActiveTab.cs
+++ Assets/BuildPopUpActiveTab.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class BuildPopUpActiveTab : MonoBehaviour
 {
     public GameObject ActiveTab;
+    public GameObject ActiveTabContent;
+    public List<GameObject> NotActiveTabContent;
     public void SetActiveTab()
     {
         ActiveTab.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+1, gameObject.transform.position.z);
-    
+        ActiveTabContent.SetActive(true);
+        NotActiveTabContent[0].SetActive(false);
+        NotActiveTabContent[1].SetActive(false);
+        NotActiveTabContent[2].SetActive(false);
+
+
     }
 }

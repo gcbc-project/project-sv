d973707f8080ab3561ac0c568bdb4c9132f6f59b
skku-sleepwalk <129564823+skku-sleepwalk@users.noreply.github.com>
2024-09-15 00:17:03 +0900

    UI (#6)
    * UI1
    
    * timeScale
    
    * UI2
    
    * ui3
    
    * UIFinish

---
+++ Assets/BuildPopUpActiveTab.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPopUpActiveTab : MonoBehaviour
+{
+    public GameObject ActiveTab;
+    public GameObject ActiveTabContent;
+    public List<GameObject> NotActiveTabContent;
+    public void SetActiveTab()
+    {
+        ActiveTab.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+1, gameObject.transform.position.z);
+        ActiveTabContent.SetActive(true);
+        NotActiveTabContent[0].SetActive(false);
+        NotActiveTabContent[1].SetActive(false);
+        NotActiveTabContent[2].SetActive(false);
+        NotActiveTabContent[3].SetActive(false);
+
+    }
+}
